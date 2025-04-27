d65215e78cff026977305515e76dc151920c2eb7
torry2 <mail@torry.to>
2025-04-27 07:23:41 +0000

    configured migrations and db

--- cashnest.py
+++ cashnest.py
@@ -1,1 +1,3 @@
-from app import app
+from app import app, db
+
+#with app.app_context(): create_all() # can be used for testing but not preferred
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32).hex()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'cashnest.db')
