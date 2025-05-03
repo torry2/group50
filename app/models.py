b1d21404768593ef60f154a5dad79e93fedcdfba
JaidenLCoder <23941237@student.uwa.edu.au>
2025-05-03 21:20:50 +0800

    3 database schema (#43)
    * added database schema
    
    * database models updated
    
    * added database schema
    
    * database models updated
    
    * models
    
    * removing garbage
    
    ---------
    
    Co-authored-by: Jaiden | La Vida Homes <jaiden@lavidahomes.com.au>

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+# List of valid categories, this field is not normalised as therefore not consistent across tables, - use this list to check validity!
+CATEGORIES = ["food", "rent", "utilities", "shopping", "entertainment", "other", "goal1", "goal2", "goal3"]
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,3 +26,32 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
+
+class Transactions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+
+    def __repr__(self):
+        return f"<Transactions {self.id} – {self.userid})>"
+
+class Financials(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    income = db.Column(db.Numeric(10, 2), nullable=False)
+    currency = db.Column(db.String(200), nullable=False)
+
+    # budget (categories: see note at top of file)
+    food = db.Column(db.Numeric(10, 2))
+    rent = db.Column(db.Numeric(10, 2))
+    utilities = db.Column(db.Numeric(10, 2))
+    shopping = db.Column(db.Numeric(10, 2))
+    entertainment = db.Column(db.Numeric(10, 2))
+    other = db.Column(db.Numeric(10, 2))
+    goal1 = db.Column(db.Numeric(10, 2))
+    goal2 = db.Column(db.Numeric(10, 2))
+
+    def __repr__(self):
+        return f"<Financials {self.id} – {self.userid})>"
