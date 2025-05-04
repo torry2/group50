0c0b25e8c054731a538c4a57e904e9eb2a2c0178
Punit Nitin Patil <161966337+Punit750@users.noreply.github.com>
2025-05-04 13:04:26 +0800

    cleared the database and added login requried

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
 from app.forms import *
 from flask import render_template , flash
-from flask_login import current_user, login_user
+from flask_login import current_user, login_required
 from app.models import User 
+from app import login
+
+@login.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
 
 
 @app.route('/')
@@ -10,19 +15,23 @@ def home():
     return render_template('home.html')
 
 @app.route('/profile')
+@login_required
 def profile():
     sf = settingsform()
     return render_template('profile.html', form=sf)
 
 @app.route('/budget')
+@login_required
 def budget():
     return render_template('budget.html')
 
 @app.route('/data')
+@login_required
 def data():
     return render_template('data.html')
 
 @app.route('/projections')
+@login_required
 def projections():
     analysis = {
         'total_projects': 10,
