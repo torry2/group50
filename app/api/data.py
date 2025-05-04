07f7b59828f6649eb788634603d359066658ef30
KLianHao <169980955+KLianHao@users.noreply.github.com>
2025-05-04 16:54:27 +0800

    Added functionality to add transactions

--- app/api/data.py
+++ app/api/data.py
@@ -1,10 +1,30 @@
 from flask import jsonify, request
 from app.forms import *
+from app import db
+from app.models import Transactions
+from flask_login import current_user
 
 ENDPOINT = "data"
 
 def data_routes(app, prefix):
-    app.add_url_rule(f"{prefix}/{ENDPOINT}", f"{prefix}/{ENDPOINT}", data, methods=["GET"])
+    app.add_url_rule(f"{prefix}/{ENDPOINT}/add", "add", add_transaction, methods=["POST"])
     
-def data():
-    return jsonify({"status": "data"}), 200
+def add_transaction():
+    if request.method != "POST":
+        return jsonify({"status":"error","message":"Method not allowed."}), 405
+    
+    form = TransactionForm(request.form)
+    if not form.validate_on_submit():
+        return jsonify({"status":"error","errors": form.errors}), 400
+    name = form.name.data
+    category = form.category.data
+    amount = form.amount.data
+
+    if None in (name, category, amount):
+        return jsonify({"status":"error","message":"Missing required fields."}), 400
+    
+    transaction = Transactions(userid=current_user.id, name=name, category=category, amount=amount)
+    db.session.add(transaction)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "Transaction added."}), 201
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for 
 from flask_wtf import FlaskForm 
-from wtforms import StringField, validators, PasswordField, SelectField, SubmitField 
-from wtforms.validators import DataRequired, Email 
+from wtforms import StringField, validators, PasswordField, SelectField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, Email, NumberRange, InputRequired
+from decimal import Decimal
 
 class settingsform(FlaskForm):
     new_password = PasswordField('Change Password', validators=[DataRequired()])
@@ -12,3 +13,9 @@ class settingsform(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class TransactionForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Food', 'Food'), ('Rent', 'Rent'), ('Utilities', 'Utilities'), ('Shopping', 'Shopping'), ('Entertainment', 'Entertainment'), ('Other', 'Other'), ('Goal1', 'Goal1'), ('Goal2', 'Goal2'), ('Goal3', 'Goal3')], validators=[DataRequired()])
+    amount = DecimalField('Amount', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    submit = SubmitField('Add')
