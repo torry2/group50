17d84dcb96e6dba772849844401c263e982db6c3
Punit Nitin Patil <161966337+Punit750@users.noreply.github.com>
2025-05-04 11:55:00 +0800

    The Login Page with Login button taking to data page functionality

--- app/api/auth.py
+++ app/api/auth.py
@@ -3,6 +3,8 @@ from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from app.forms import *
+
 
 ENDPOINT = "auth"
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from app.forms import *
-from flask import render_template
+from flask import render_template , flash
+from flask_login import current_user, login_user
+from app.models import User 
 
 
 @app.route('/')
@@ -35,6 +37,20 @@ def projections():
     ]
 
     return render_template('projections.html', analysis=analysis, projects=projects)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    if current_user.is_authenticated: 
+        return redirect(url_for('data')) 
+    form = LoginForm() 
+    if form.validate_on_submit(): 
+        user = User.query.filter_by(username=form.username.data).first() 
+        if user is None or not user.check_password(form.password.data): 
+            flash('Invalid username or password') 
+            return redirect(url_for('login')) 
+        return redirect(url_for('data')) 
+    return render_template('login.html', title='Sign In', form=form)
+ 
 
 
 
