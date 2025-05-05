c14a22721fa32422d25bc185ac819b1c4ef04b4a
KLianHao <169980955+KLianHao@users.noreply.github.com>
2025-05-05 22:02:49 +0800

    Added API for setting income & budgets

--- app/forms.py
+++ app/forms.py
@@ -16,9 +16,23 @@ class LoginForm(FlaskForm):
 
 class TransactionForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    category = SelectField('Category', choices=[('Food', 'Food'), ('Rent', 'Rent'), ('Utilities', 'Utilities'), ('Shopping', 'Shopping'), ('Entertainment', 'Entertainment'), ('Other', 'Other'), ('Goal1', 'Goal1'), ('Goal2', 'Goal2'), ('Goal3', 'Goal3')], validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Food', 'Food'), ('Rent', 'Rent'), ('Utilities', 'Utilities'), ('Shopping', 'Shopping'), ('Entertainment', 'Entertainment'), ('Other', 'Other'), ('Goal 1', 'Goal 1'), ('Goal 2', 'Goal 2'), ('Goal 3', 'Goal 3')], validators=[DataRequired()])
     amount = DecimalField('Amount', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
     submit = SubmitField('Add')
 
 class ProjectionsForm(FlaskForm):
     submit = SubmitField('Get Projections')
+
+class IncomeBudgetForm(FlaskForm):
+    income = DecimalField('Income', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    currency = SelectField('Currency', choices=[('AUD', 'AUD'), ('USD', 'USD')], validators=[DataRequired()])
+    food_budget = DecimalField('Food', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    rent_budget = DecimalField('Rent', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    utilities_budget = DecimalField('Utilities', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    shopping_budget = DecimalField('Shopping', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    entertainment_budget = DecimalField('Entertainment', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    other_budget = DecimalField('Other', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    goal1_budget = DecimalField('Goal 1', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    goal2_budget = DecimalField('Goal 2', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    goal3_budget = DecimalField('Goal 3', places=2, rounding=None, validators=[InputRequired(), NumberRange(min=Decimal('0.00'))])
+    submit = SubmitField('Save')
