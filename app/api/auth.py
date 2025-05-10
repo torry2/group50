e4a5061f0bf712d09e19b8efbf30df87f44fbb15
Lian Hao Kueh <169980955+KLianHao@users.noreply.github.com>
2025-05-10 21:43:12 +0800

    Removed code that accepted JSON data input (#68)

--- app/api/auth.py
+++ app/api/auth.py
@@ -27,17 +27,12 @@ def login():
     if request.method != "POST":
         return jsonify({"status": "error", "message": "Method not allowed."}), 405
 
-    if request.is_json: # support json and wt-forms as the frontend isnt implemented yet (yes this allows for csrf bypass once implemented, but is being used for testing)
-        data = request.get_json()
-        username = data.get("username")
-        password = data.get("password")
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
     else:
-        form = LoginForm(request.form)
-        if form.validate_on_submit():
-            username = form.username.data
-            password = form.password.data
-        else:
-            return jsonify({"status": "error", "message": "Invalid form data."}), 400
+        return jsonify({"status": "error", "message": "Invalid form data."}), 400
 
     user = User.query.filter_by(username=username).first()
 
@@ -51,17 +46,12 @@ def register():
     if request.method != "POST":
         return jsonify({"status": "error", "message": "Method not allowed."}), 405
 
-    if request.is_json:
-        data = request.get_json()
-        username = data.get("username")
-        password = data.get("password")
-    else: 
-        form = LoginForm()
-        if form.validate_on_submit():
-            username = form.username.data
-            password = form.password.data
-        else:
-            return jsonify({"status": "error", "message": "Invalid form data."}), 400
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+    else:
+        return jsonify({"status": "error", "message": "Invalid form data."}), 400
 
     if User.query.filter_by(username=username).first(): # forced unique by the model
         return jsonify({"status": "error", "message": "Username already exists."}), 409
@@ -81,17 +71,13 @@ def update():
     if not current_user.is_authenticated:
         return jsonify({"status": "error", "message": "User not authenticated."}), 401
 
-    if request.is_json:
-        data = request.get_json()
-        new_username = data.get("username")
-        new_password = data.get("password")
-    else:  
-        form = UpdateForm(request.form)
-        if form.validate_on_submit():
-            new_username = form.username.data
-            new_password = form.password.data
-        else:
-            return jsonify({"status": "error", "message": "Invalid form data."}), 400
+
+    form = UpdateForm(request.form)
+    if form.validate_on_submit():
+        new_username = form.username.data
+        new_password = form.password.data
+    else:
+        return jsonify({"status": "error", "message": "Invalid form data."}), 400
 
     if new_username:
         if User.query.filter_by(username=new_username).first():
